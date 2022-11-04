c44ff976ccfb92629e092ea6fead8ae82bd2ca73
Danny Echeverri <decheverri123@gmail.com>
2022-11-04 11:18:04 -0400

    form submission

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  login() {
+    console.log(this.credentials);
+  }
+
   credentials = {
     email: '',
     password: '',
