42df55b3c3ccb8f2aeaa6d467fedbb47b70f2866
Danny Echeverri <decheverri123@gmail.com>
2022-11-08 20:00:26 -0500

    login works

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -6,19 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  login() {
-    console.log(this.credentials);
-  }
+  constructor(private auth: AngularFireAuth) {}
+
+  ngOnInit(): void {}
 
   credentials = {
     email: '',
     password: '',
   };
 
+  showAlert = false;
+  alertMessage = 'Please wait.';
+  alertColor = 'blue';
+  inSubmission = false;
+
   emailValidationPattern =
     "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
 
-  constructor() {}
+  async login() {
+    this.showAlert = true;
+    this.alertMessage = 'Please wait...';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
 
-  ngOnInit(): void {}
+    try {
+      this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (error) {
+      this.inSubmission = false;
+      this.alertMessage = 'Error occurred.';
+      this.alertColor = 'red';
+      return;
+    }
+    this.alertMessage = 'Success! You are now logged in.';
+    this.alertColor = 'green';
+  }
 }
