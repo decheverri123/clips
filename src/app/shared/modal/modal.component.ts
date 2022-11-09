2536cb67b9d5b55d3e46ac0826a03c7249c541f7
Danny Echeverri <decheverri123@gmail.com>
2022-11-08 20:03:38 -0500

    modal destruction

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, OnDestroy } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   @Input() modalId = '';
 
   closeModal() {
@@ -14,8 +14,11 @@ export class ModalComponent implements OnInit {
   }
 
   constructor(public modal: ModalService, public el: ElementRef) {}
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
-    document.body.appendChild(this.el.nativeElement);
+    document.body.removeChild(this.el.nativeElement);
   }
 }
