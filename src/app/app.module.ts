44fc8dc6161bb8fb0651624bc20f5cb4f6c95f42
Danny Echeverri <decheverri123@gmail.com>
2022-11-10 11:27:27 -0500

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,9 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { HomeComponent } from './home/home.component';
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
