a6287c14bb4f4076ba96b2d7913bdbc2e1d376e4
Danny Echeverri <decheverri123@gmail.com>
2022-11-11 11:21:31 -0500

    progress given

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -16,6 +16,7 @@ export class UploadComponent implements OnInit {
   alertColor = 'blue';
   alertMessage = 'Please wait. Your clip is being uploaded.';
   inSubmission = false;
+  percentage = 0;
 
   title = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
@@ -50,6 +51,9 @@ export class UploadComponent implements OnInit {
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
 
-    this.storage.upload(clipPath, this.file);
+    const task = this.storage.upload(clipPath, this.file);
+    task.percentageChanges().subscribe((progress) => {
+      this.percentage = (progress as number) / 100;
+    });
   }
 }
