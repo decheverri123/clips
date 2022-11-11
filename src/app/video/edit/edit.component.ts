25db5f8943e6f1b783d11d40303d45235f9c703d
Danny Echeverri <decheverri123@gmail.com>
2022-11-11 14:13:25 -0500

    edit modal

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { ModalService } from '../../services/modal.service';
+import IClip from '../../models/clip.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css'],
 })
-export class EditComponent implements OnInit {
+export class EditComponent implements OnInit, OnChanges {
+  @Input() activeClip: IClip | null = null;
+
+  clipID = new FormControl('');
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+  editForm = new FormGroup({
+    title: this.title,
+  });
+
   constructor(private modal: ModalService) {}
 
   ngOnInit(): void {
     this.modal.register('editClip');
+  }
+
+  ngOnChanges() {
+    if (!this.activeClip) {
+      return;
+    }
+    this.clipID.setValue(this.activeClip.docID as string);
+    this.title.setValue(this.activeClip.title);
   }
 }
