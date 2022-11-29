82fb8ecec6e9667d547ba90732e5cd00fcff416a
Danny Echeverri <decheverri123@gmail.com>
2022-11-29 16:36:53 -0500

    removed videojs

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import videojs from 'video.js';
 
 @Component({
   selector: 'app-clip',
@@ -10,12 +15,10 @@ import videojs from 'video.js';
 export class ClipComponent implements OnInit {
   id = '';
   @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
-  player?: videojs.Player;
 
   constructor(public route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.player = videojs(this.target?.nativeElement);
     this.route.params.subscribe((params: Params) => {
       this.id = params.id;
     });
